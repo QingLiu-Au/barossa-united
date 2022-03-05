c4faff25cfa6c09d57f73299e8419cf73cbe5021
Hao Chen <hao.chen4@student.tafesa.edu.au>
2022-03-05 14:19:21 +1030

    initial commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="navigation">
+        <div  v-for="m in menu" v-bind:key="m.name">
+            <router-link class="navi-link" :to="m.to">{{m.name}}</router-link>
+        </div>
+        <img v-if="showImage" src="http://www.barossaunited.com.au/images/BUFCShield_CMYK_FB-01.png" height="300px" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navigation',
+    props: {
+        showImage: Boolean
+    },
+    data() {
+        return {
+            menu: [
+                {
+                    to: "/",
+                    name: "Home"
+                },
+                {
+                    to: "/presidents-welcome",
+                    name: "Presidents Welcome"
+                },
+                {
+                    to: "/fees-registration",
+                    name: "Fees & Registration"
+                },
+                {
+                    to: "/player-info",
+                    name: "Player Info"
+                },
+                {
+                    to: "/history",
+                    name: "History"
+                },
+                {
+                    to: "/policies",
+                    name: "Policies"
+                },
+                {
+                    to: "/newsletters",
+                    name: "Newsletters"
+                },
+                {
+                    to: "/contact",
+                    name: "Contact"
+                }
+            ]            
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+img {
+    margin: 50px 0px 0px 100px;
+}
+.navigation {
+    margin-top: 50px;
+    float: left;
+    width: 30%;
+}
+.navi-link {
+    text-decoration: none;
+    margin-left: 100px;
+    font-size: 1.8em;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="home">
+    <section>
+      <Navigation id="navi" :showImage="true" />
+      <article>
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+      </article>
+    </section>
+
+    <footer>
+      <p>Footer</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import Navigation from '../components/Navigation.vue';
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Navigation,
+  },
+};
+</script>
+<style scoped>
+/* Create two columns/boxes that floats next to each other */
+#navi {
+  float: left;
+  width: 30%;
+  background: #fff;
+}
+
+/* Style the list inside the menu */
+#navi ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+article {
+  float: left;
+  width: 70%;
+  background-color: #f1f1f1;
+}
+
+/* Clear floats after the columns */
+section::after {
+  content: '';
+  display: table;
+  clear: both;
+}
+
+/* Style the footer */
+footer {
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+
+/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
+@media (max-width: 600px) {
+  nav,
+  article {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
