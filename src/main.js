63f3e65b603a29828733184161c27d754342ee25
Hao Chen <hao.chen4@student.tafesa.edu.au>
2022-03-05 20:19:25 +1030

    introduce new rich text editor

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueHtml5Editor from "vue-html5-editor";
 
 Vue.config.productionTip = false;
-Vue.use(VueHtml5Editor);
 
 new Vue({
   router,
--- src/views/Admin-Edit.vue
+++ src/views/Admin-Edit.vue
@@ -1,9 +1,21 @@
 <template>
-    <VueHtml5Editor />
+  <div id="app">
+    <vue-editor v-model="content"></vue-editor>
+  </div>
 </template>
 
 <script>
+import { VueEditor } from "vue2-editor";
+
 export default {
-    
-}
+  components: {
+    VueEditor,
+  },
+
+  data() {
+    return {
+      content: "",
+    };
+  },
+};
 </script>
