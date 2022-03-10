b80ff284d0379a7e21fff52f477ab1eddbf52872
Hao Chen <hao.chen4@student.tafesa.edu.au>
2022-03-10 22:52:38 +1030

    add some animation options

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,124 @@
 <template>
   <div id="app">
-    <router-view />
+    <!-- <router-view /> -->
+    <!-- name="fade" -->
+    <transition
+      @beforeLeave="beforeLeave"
+      @enter="enter"
+      @afterEnter="afterEnter"
+      :name="transitionName"
+      mode="out-in"
+    >
+      <router-view :key="key" />
+    </transition>
   </div>
 </template>
 
 <script>
 import Navigation from "./components/Navigation.vue";
+const DEFAULT_TRANSITION = "fade";
 export default {
+  data() {
+    return {
+      prevHeight: 0,
+      transitionName: DEFAULT_TRANSITION,
+    };
+  },
+  computed: {
+    key() {
+      return this.$route.path.replace(/\//g, "_");
+    },
+  },
   components: {
     Navigation,
+  },
+  created() {
+    this.$router.beforeEach((to, from, next) => {
+      let transitionName = to.meta.transitionName || from.meta.transitionName;
+
+      if (transitionName === "slide") {
+        const toDepth = to.path.split("/").length;
+        const fromDepth = from.path.split("/").length;
+        transitionName = toDepth < fromDepth ? "slide-right" : "slide-left";
+      }
+
+      this.transitionName = transitionName || DEFAULT_TRANSITION;
+
+      next();
+    });
+  },
+  methods: {
+    beforeLeave(element) {
+      this.prevHeight = getComputedStyle(element).height;
+    },
+    enter(element) {
+      const { height } = getComputedStyle(element);
+
+      element.style.height = this.prevHeight;
+
+      setTimeout(() => {
+        element.style.height = height;
+      });
+    },
+    afterEnter(element) {
+      element.style.height = "auto";
+    },
   },
 };
 </script>
 <style>
 body {
   margin: 0px;
+}
+
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition-duration: 0.5s;
+  transition-property: height, opacity, transform;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+  overflow: hidden;
+}
+
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate(2em, 0);
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 0;
+  transform: translate(-2em, 0);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.5s;
+  transition-property: height, opacity;
+  transition-timing-function: ease;
+  overflow: hidden;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(200px);
+}
+.route-enter-active {
+  transition: all 0.5s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-200px);
+}
+.route-leave-active {
+  transition: all 0.5s ease-in;
 }
 /* 
 #app {
