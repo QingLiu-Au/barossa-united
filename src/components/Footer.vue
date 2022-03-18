bb91ac46000e0344e29667da463884ccf7663980
Qing Liu <qingliuhelen@gmail.com>
2022-03-18 23:51:05 +1030

    upload file

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,20 +2,25 @@
   <footer class="fixed-bottom" style="background-color: #212121">
     <div class="container">
       <div class="row text-center text-white text-capitalize">
-        <div class="col-lg-5 col-md-4 mb-2 mb-md-0">
+        <div class="col-lg-6 col-md-4 mb-2 mb-md-0">
           connect with us on social media
         </div>
-        <div class="col-lg-7 col-md-6 mb-2 mb-md-0">
-          <a href="#"
-            ><font-awesome-icon :icon="['fab', 'facebook-square']"
-          /></a>
-
-          <a href="#"
-            ><font-awesome-icon :icon="['fab', 'twitter-square']"
-          /></a>
-          <a href="#"
-            ><font-awesome-icon :icon="['fab', 'instagram-square']"
-          /></a>
+        <div class="col-lg-6 col-md-6 mb-2 mb-md-0">
+          <div class="d-inline-flex p-2">
+            <a href="#"
+              ><font-awesome-icon :icon="['fab', 'facebook-square']"
+            /></a>
+          </div>
+          <div class="d-inline-flex p-2">
+            <a href="#"
+              ><font-awesome-icon :icon="['fab', 'twitter-square']"
+            /></a>
+          </div>
+          <div class="d-inline-flex p-2">
+            <a href="#"
+              ><font-awesome-icon :icon="['fab', 'instagram-square']"
+            /></a>
+          </div>
         </div>
       </div>
     </div>
