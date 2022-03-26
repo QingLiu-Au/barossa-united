9521a69caf2b8dae8f54bd046819ef373d8db828
Qing Liu <qingliuhelen@gmail.com>
2022-03-26 17:14:04 +1030

    add vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-// module.exports = {
-//   devserver: {
-//     disableHostCheck: true,
-//   },
-// };
+const path = require("path");
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+    port: process.env.PORT,
+  },
+};
