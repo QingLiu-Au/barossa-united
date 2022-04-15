0935b61acb6234de9ececa26d06eb55f590af900
Qing Liu <qingliuhelen@gmail.com>
2022-04-15 23:58:58 +0930

    update page route

--- src/components/Admin/Page.vue
+++ src/components/Admin/Page.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="container">
+  <div class="container text-black">
     <div class="text-center">
       <h3>Pages Manage</h3>
     </div>
     <div class="mb-3" v-for="(page, i) in pages" :key="i">
-      <input type="text" class="mr-5" :value="page.RouteName" disabled />
-      <input type="radio" v-model="page.Public" value="0" /> Hide
-      <input type="radio" v-model="page.Public" value="1" /> Show
+      <input type="text" class="mr-5" v-model="page.RouteName" />
+      <input type="radio" v-model="page.Hidden" value="0" /> Show
+      <input type="radio" v-model="page.Hidden" value="1" /> Hidden
       <!-- <span><strong>{{ page.RouteName }}</strong></span> -->
+    </div>
+    <div class="text-center">
+      <button type="button" class="btn btn-success" v-on:click="update">
+        Update
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "PageManage",
   data() {
@@ -24,6 +30,10 @@ export default {
     this.pages = this.$store.state.routes;
   },
 
-  methods: {},
+  methods: {
+    async update() {
+      await axios.post("/ManageRoute.php", JSON.stringify(this.pages));
+    },
+  },
 };
 </script>
